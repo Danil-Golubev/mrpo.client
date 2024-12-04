a15f4caca4d32a87ed2965edbd4792bdf453865a
Danil-Golubev <dokers190rus@mail.ru>
2024-12-04 16:54:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+type user = {
+  tgId:string,
+  firstName:string,
+  secondName:string
+}
 function App() {
+  const [tgId, setIgId] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+
+const handleReg = ()=>{
+  // const object:user= {
+  //   // tgId:windo
+  // }
+}
+
+useEffect(() => {
+  // Инициализация WebApp
+
+
+  const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+
+  if (initDataUnsafe && initDataUnsafe.user && initDataUnsafe.user.username) {
+    setFirstName(initDataUnsafe.user.username); // Установка username в состояние
+  } else {
+    setFirstName("Username не установлен");
+  }
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className='regList'>
+<p className='mainTitle'>Регистрация на мероприятие</p>
+  <div>
+      <p>Введи имя</p>
+      <input onChange={(value)=>setFirstName(value.target.value as unknown as string)} className='input'></input></div>
+      <div>
+   
+      <p>Введи Фамилию</p>
+      <p>Введи имя</p>
+      <input onChange={(value)=>setSecondName(value.target.value as unknown as string)} className='input'></input>
+      </div>
+      <button className='button'>Зарегестрироваться</button>
+      </div>
     </div>
   );
 }
---
+++ src/telegram-webapp.d.ts
@@ -0,0 +1,36 @@
+declare global {
+    interface Window {
+      Telegram: {
+        WebApp: {
+          ready: () => void;
+          close: () => void;
+          initData: string;
+          initDataUnsafe: {
+            user?: {
+              id: number;
+              first_name: string;
+              last_name?: string;
+              username?: string;
+              language_code?: string;
+            };
+          };
+          themeParams: Record<string, string>;
+          MainButton: {
+            text: string;
+            color?: string;
+            textColor?: string;
+            isVisible: boolean;
+            setText: (text: string) => void;
+            show: () => void;
+            hide: () => void;
+            enable: () => void;
+            disable: () => void;
+            onClick: (callback: () => void) => void;
+          };
+        };
+      };
+    }
+  }
+  
+  export {};
+  
