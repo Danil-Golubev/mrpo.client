f7a977a347964216ec0aed59c9f67f947eb5c62c
Danil-Golubev <dokers190rus@mail.ru>
2024-12-04 20:51:37 +0300

    fixes

--- src/api.ts
+++ src/api.ts
@@ -5,12 +5,12 @@ export const fetchPostUser= async (req: user) => {
     tgId:req.tgId,
     firstName:req.firstName,
     secondName:req.secondName}
-	const res = await axios.post('https://mrpo-backend.onrender.com/reg-user', data);
+	const res = await axios.post('http://localhost:4444/reg-user', data);
 	return res.data;
 };  
 
 
 export const fetchCheckReg= async (req:any) => {
-	const res = await axios.post('https://mrpo-backend.onrender.com/bytgId', req);
+	const res = await axios.post('http://localhost:4444/bytgId', req);
 	return res.data;
 };  
